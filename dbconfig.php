59788676446f41bcf8120b847c9309abdeec19cb
mturde <muhammedturde@outlook.com>
2021-03-11 12:55:39 +0300

    Website Files

---
+++ dbconfig.php
@@ -0,0 +1,11 @@
+<?php
+/****************************************************/
+/************** Created by : Vivek Gupta ************/
+/***************     www.vsgupta.in     *************/
+/***************     www.iotmonk.com     *************/
+/****************************************************/ 
+define('DB_USER', "muhammed_1234");     // Your database user name
+define('DB_PASSWORD', "muhammed586");			// Your database password (mention your db password here)
+define('DB_DATABASE', "muhammed_deneme"); // Your database name
+define('DB_SERVER', "cloud.zirvesunucum.com");			// db server (Mostly will be 'local' host)
+?>
